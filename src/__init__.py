a53b6a48c0fbde7f8002478bb55bcbb2167c2ce5
saumyaraj-commit <saumya.raj@yale.edu>
2026-02-22 17:41:12 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,13 @@
-from src.schedule import TraySwitch, build_schedule
+from .scheduler import TraySwitch, build_plan, reschedule_from, PlanInput
+from .calendar_ics import write_ics
+from .storage import load_plan, save_plan
 
-__all__ = ["TraySwitch", "build_schedule"]
+__all__ = [
+    "TraySwitch",
+    "PlanInput",
+    "build_plan",
+    "reschedule_from",
+    "write_ics",
+    "load_plan",
+    "save_plan",
+]
